6837529ab47e080017782ddb8579d9b16f0b09f6
Ho Nyeong <dev.honing@gmail.com>
2024-03-15 14:43:15 +0900

    Refactor: Flask -> FastAPI로 Model 서버 변경

--- test/Model/app.py
+++ test/Model/app.py
@@ -1,14 +1,13 @@
 # Model/app.py
 
-from flask import Flask, jsonify
+from fastapi import FastAPI
 from main import json_data
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
+@app.get("/")
 def get_prediction():
     # main.py에서 생성된 JSON 데이터를 클라이언트에 반환
-    return jsonify(json_data)
+    return json_data
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# 실행 명령: uvicorn app:app --reload
