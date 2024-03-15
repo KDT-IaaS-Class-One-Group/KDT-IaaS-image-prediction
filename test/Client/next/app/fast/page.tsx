a882e7eabbf892944de0dd5528895e3494b0c2aa
YoonJunHyeon <qmdldos@gmail.com>
2024-03-15 16:21:43 +0900

    test : 통신 테스트완료

--- test/Client/next/app/fast/page.tsx
+++ test/Client/next/app/fast/page.tsx
@@ -1,6 +1,15 @@
 // Client/next/app/fast/page.tsx
+"use client";
+import React, { useEffect } from "react";
 
 const FastPage = () => {
+  useEffect(() => {
+    fetch("http://localhost:8000")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <main>
       <h1>03 REST API Server(FastAPI)</h1>
--- test/Server/app.py
+++ test/Server/app.py
@@ -1,8 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 # Server/app.py
 
-from fastapi import FastAPI
-
 app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
