aa6b1edc2f300f7d29d716f88bc812cd320f6798
Ho Nyeong <dev.honing@gmail.com>
2024-03-15 12:14:48 +0900

    Feat: 메인 페이지 컴포넌트 제작
    - a 태그를 통한 라우트 경로 구상

---
+++ test/server/app/page.tsx
@@ -0,0 +1,25 @@
+export default function MainPage() {
+  return (
+    <main>
+      <h1>Project B 인터랙션 점검</h1>
+      <h2>
+        <a href="/user">01 User(Client)</a>
+      </h2>
+      <h2>
+        <a href="/app">02 App Server(Next App)</a>
+      </h2>
+      <h2>
+        <a href="/fast">03 REST API Server(FastAPI)</a>
+      </h2>
+      <h2>
+        <a href="/database">04 DataBase Server(MariaDB)</a>
+      </h2>
+      <h2>
+        <a href="/storage">05 Storage Server(S3)</a>
+      </h2>
+      <h2>
+        <a href="/ml">06 Machine Learning Model</a>
+      </h2>
+    </main>
+  );
+}
