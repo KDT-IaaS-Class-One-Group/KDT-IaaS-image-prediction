5022eee0de30d4e70299df1bb9748ef206d514ff
YoonJunHyeon <qmdldos@gmail.com>
2024-03-19 19:08:34 +0900

    feat : 테스트 라우트 설정 form데이터에 이미지를 받는 로직 추가 - form 데이터를 받진 못해서 다시 찾아봐야 함

--- test/Server/app.py
+++ test/Server/app.py
@@ -1,6 +1,6 @@
 # Server/app.py
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import httpx
 
 app = FastAPI()
@@ -26,9 +26,24 @@ async def get_table_names():
     table_names = response.json()  # JSON 데이터 추출
     return table_names
 
+# Form을 사용하여 클라이언트로부터 데이터를 받음
+# @app.post("/check")
+# async def check_data(data: str = Form(...)):
+#     return {"data": data}
+
 @app.post("/check")
-async def check_request(request_body: dict):
-    print(request_body)
-    return {"message": "Request body printed successfully"}
+async def check_file_upload(image: UploadFile = File(...)):
+    # 파일 메타데이터를 콘솔에 출력
+    file_details = {
+        "filename": image.filename,
+        "content_type": image.content_type,
+        "file_size": await image.read().__sizeof__()
+    }
+    print(file_details)
+
+    # 파일 처리 완료 후 파일을 닫습니다.
+    await image.close()
+
+    return {"file_details": file_details}
 
 # 실행 명령: uvicorn app:app --reload --port 8000
