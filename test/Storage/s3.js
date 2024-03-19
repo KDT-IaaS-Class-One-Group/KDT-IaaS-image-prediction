c8e4adcbcefd33cab0a330b889c353b0a985633e
Ho Nyeong <dev.honing@gmail.com>
2024-03-19 14:17:07 +0900

    Feat: 환경변수 로드 및 AWS S3 설정

--- test/Storage/s3.js
+++ test/Storage/s3.js
@@ -2,10 +2,19 @@
 
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const AWS = require('aws-sdk');
 
-app.get('/', (req, res) => {
-  res.send("Express 서빙 테스트 완료");
-})
+// 환경변수 로드
+dotenv.config();
+
+// AWS S3 설정
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
 
 // 서버를 5555번 포트로 실행
 const PORT = 5555;
