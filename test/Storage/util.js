66a5235e427f16434232a72c6e035b27f3cb4dd3
Ho Nyeong <dev.honing@gmail.com>
2024-03-19 16:29:15 +0900

    Comments: JSDoc으로 구상한 함수의 설명을 추가

--- test/Storage/util.js
+++ test/Storage/util.js
@@ -1,5 +1,10 @@
 // test/Storage/util.js
 
+/**
+ * @function ObjectsInBucketCRUD - 버킷 내 객체의 생성, 조회, 삭제를 수행하는 유틸 함수
+ * @param {string} operation - create, read, delete
+ * @param {string} fileName - 파일명
+ */
 function ObjectsInBucketCRUD(operation, fileName) {
   if (operation === "create") {
     console.log(`선택한 내용을 아래에서 확인하세요. \n작동 모드: ${operation} / 파일명: ${fileName}`);
