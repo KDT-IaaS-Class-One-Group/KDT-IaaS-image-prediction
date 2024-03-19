013e291f4f8ebad4687074b9d828bbf1dc0e6f6d
ChaeYi <chaeyi990731@gmail.com>
2024-03-19 16:29:58 +0900

    Feat: 날짜형식 파일명 추가
    날짜형식으로 된 유틸함수 가져와서 파일 이름을 경로와 업로드 날짜 파일이름으로 저장

--- test/utils/upload/s3/uploadFileToS3.js
+++ test/utils/upload/s3/uploadFileToS3.js
@@ -1,12 +1,14 @@
 const { PutObjectCommand } = require('@aws-sdk/client-s3');
 const s3Client = require('../../Config/s3Config');
+const appendDateToFileName = require('./appendDateToFileName');
 const fs = require('fs');
 
 async function uploadFileToS3(filePath, fileName, contentType) {
+  const datedFileName = appendDateToFileName(fileName); // 파일명에 날짜 추가
   const fileStream = fs.createReadStream(filePath);
   const uploadParams = {
     Bucket: process.env.S3_BUCKET,
-    Key: `upload/${fileName}`,
+    Key: `upload/${datedFileName}`,
     Body: fileStream,
     ContentType: contentType,
   };
@@ -14,7 +16,7 @@ async function uploadFileToS3(filePath, fileName, contentType) {
     const command = new PutObjectCommand(uploadParams);
     const response = await s3Client.send(command);
     console.log('Successfully uploaded file to S3:', response);
-    return `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/upload/${fileName}`;
+    return `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/upload/${datedFileName}`;
   } catch (error) {
     console.error('Error uploading file to S3:', error);
     throw error;
