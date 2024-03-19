7176e50d372d3873acdcc94debc4c94a67233f52
Ho Nyeong <dev.honing@gmail.com>
2024-03-19 16:50:59 +0900

    Feat: 유틸 함수로 S3 스토리지에 test.txt 저장 성공

--- test/Storage/util.js
+++ test/Storage/util.js
@@ -8,6 +8,31 @@
 function ObjectsInBucketCRUD(operation, fileName) {
   if (operation === "create") {
     console.log(`${operation} 실행 -> ${fileName}`)
+
+    const AWS = require('aws-sdk');
+    const dotenv = require('dotenv');
+    dotenv.config({path: "../../.env"});
+    const s3 = new AWS.S3(
+      {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+      }
+    );
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileName,
+      Body: "something"
+    };
+
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        console.log("객체 생성 중 오류 발생:", err);
+      } else {
+        console.log("객체가 성공적으로 생성되었습니다.", data);
+      }
+    });
+    
   }
   else if (operation === "read") {
     console.log(`${operation} 실행 -> ${fileName}`)
@@ -22,7 +47,4 @@ function ObjectsInBucketCRUD(operation, fileName) {
   }
 }
 
-ObjectsInBucketCRUD("create", "test.txt"); // create 실행 -> test.txt
-ObjectsInBucketCRUD("read", "test.txt"); // read 실행 -> test.txt
-ObjectsInBucketCRUD("delete", "test.txt"); // delete 실행 -> test.txt
-ObjectsInBucketCRUD("update", "test.txt"); // 잘못된 작동 모드입니다. JSDoc의 설명에 맞게 다시 시도해보세요.
+ObjectsInBucketCRUD("create", "test.txt");
