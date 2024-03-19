c529171aaa67d84bd1a64b93d8ef5c2837c019f0
ChaeYi <chaeyi990731@gmail.com>
2024-03-19 16:33:56 +0900

    Rename : console 메세지 한글화

--- test/utils/upload/db/saveMetadataToDb.js
+++ test/utils/upload/db/saveMetadataToDb.js
@@ -3,22 +3,22 @@ const mysql = require('mysql2/promise');
 const dbConfig = require('./dbConfig'); // dbConfig 모듈 불러오기
 
 async function saveMetadataToDb(file_name, file_size, content_type, s3_url) {
-    const connection = await mysql.createConnection(dbConfig);
-    
-    const query = `
+  const connection = await mysql.createConnection(dbConfig);
+
+  const query = `
         INSERT INTO file_metadata (file_name, file_size, content_type, s3_url) 
         VALUES (?, ?, ?, ?)
     `;
-    
-    try {
-        const [results] = await connection.execute(query, [file_name, file_size, content_type, s3_url]);
-        console.log('Metadata saved to DB:', results);
-    } catch (error) {
-        console.error('Error saving metadata to DB:', error);
-        throw error;
-    } finally {
-        await connection.end();
-    }
+
+  try {
+    const [results] = await connection.execute(query, [file_name, file_size, content_type, s3_url]);
+    console.log('DB에 메타데이터가 담겼습니다:', results);
+  } catch (error) {
+    console.error('DB에 메타데이터를 담지 못했습니다:', error);
+    throw error;
+  } finally {
+    await connection.end();
+  }
 }
 
 module.exports = saveMetadataToDb;
