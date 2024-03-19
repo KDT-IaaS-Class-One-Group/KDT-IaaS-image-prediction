149bb1aa63fa56c0d2bec0fdf24e08f7226a0905
ChaeYi <chaeyi990731@gmail.com>
2024-03-19 13:04:46 +0900

    Feat:환경변수를 사용한 DB 접속 함수 생성

---
+++ test/utils/Config/dbConfig.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const dbConfig = {
+  host: process.env.DB_HOST || '',
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || '',
+};
+
+export default dbConfig;
