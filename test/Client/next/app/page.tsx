eadce18807e879356e6041546343ef2d9b60d341
YoonJunHyeon <qmdldos@gmail.com>
2024-03-19 17:52:44 +0900

    feat : 컴포넌트 적용

--- test/Client/next/app/page.tsx
+++ test/Client/next/app/page.tsx
@@ -1,27 +1,11 @@
-// Client/next/app/layout.tsx
+"use client";
+import React, { useState } from "react";
+import ImageUploadForm from "@/app/component/ImageUploadForm";
 
 export default function MainPage() {
   return (
     <main>
-      <h1>Project B 인터랙션 점검</h1>
-      <h2>
-        <a href="/user">01 User</a>
-      </h2>
-      <h2>
-        <a href="/next">02 App Server(Next.js)</a>
-      </h2>
-      <h2>
-        <a href="/fast">03 REST API Server(FastAPI)</a>
-      </h2>
-      <h2>
-        <a href="/db">04 DataBase Server(MariaDB)</a>
-      </h2>
-      <h2>
-        <a href="/storage">05 Storage Server(S3)</a>
-      </h2>
-      <h2>
-        <a href="/ml">06 Machine Learning Model</a>
-      </h2>
+      <ImageUploadForm />
     </main>
   );
 }
