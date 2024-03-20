afbe74ca9cbd7eb778418f5618cad0ef1d4f5196
YoonJunHyeon <qmdldos@gmail.com>
2024-03-20 14:47:38 +0900

    test : 파일 핸들링 라우팅 설정

--- test/Server/app.py
+++ test/Server/app.py
@@ -1,8 +1,8 @@
-# Server/app.py
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 import httpx
 
+# Server/app.py
 app = FastAPI()
 
 app.add_middleware(
@@ -16,6 +16,7 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
+    print("Hello, FastAPI!")
     return {"Greet": "Hello, FastAPI!"}
 
 @app.get("/table")
@@ -26,10 +27,10 @@ async def get_table_names():
     table_names = response.json()  # JSON 데이터 추출
     return table_names
 
-# Form을 사용하여 클라이언트로부터 데이터를 받음
-# @app.post("/check")
-# async def check_data(data: str = Form(...)):
-#     return {"data": data}
+@app.post("/test")
+async def handle_post_request(data: dict):
+    print(data)
+    return {"message": "Data received successfully"}
 
 @app.post("/check")
 async def check_file_upload(image: UploadFile = File(...)):
@@ -45,5 +46,35 @@ async def check_file_upload(image: UploadFile = File(...)):
     await image.close()
 
     return {"file_details": file_details}
+
+@app.post("/form")
+async def check_data(data: str = Form(...)):
+    return {"data": data}
+
+@app.post("/check1")
+async def check_file_upload2(image: UploadFile = File(...)):
+    # 파일 처리 로직...
+    print(image)
+    # 파일을 저장할 경로 설정
+    save_path = r"C:\Users\user\Desktop\k-digital\DEV\KDT-IaaS-image-prediction\test\Server\images\\" + image.filename
+
+    # 이미지 파일 저장
+    with open(save_path, "wb") as f:
+        content = await image.read()
+        f.write(content)
+
+    return {"message": "Image saved successfully"}
+
+
+@app.post("/files/")
+async def create_file(
+    file: bytes = File(), fileb: UploadFile = File(), token: str = Form()
+):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
+
 
 # 실행 명령: uvicorn app:app --reload --port 8000
