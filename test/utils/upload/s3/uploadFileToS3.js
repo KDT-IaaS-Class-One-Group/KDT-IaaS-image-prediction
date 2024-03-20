3691457d548f1f351a37a43482a7603d3d485361
ChaeYi <chaeyi990731@gmail.com>
2024-03-20 12:54:26 +0900

    Fix: retrun을 bucketName으로 통일

--- test/utils/upload/s3/uploadFileToS3.js
+++ test/utils/upload/s3/uploadFileToS3.js
@@ -1,10 +1,11 @@
 require('dotenv').config({ path: '../../../.env' });
+const bucketName = process.env.S3_BUCKET;
+console.log(bucketName);
+
 const { PutObjectCommand } = require('@aws-sdk/client-s3');
 const s3Client = require('../../Config/s3Config');
 const appendDateToFileName = require('./appendDateToFileName');
 const fs = require('fs');
-const bucketName = process.env.S3_BUCKET;
-console.log(bucketName);
 
 async function uploadFileToS3(filePath, fileName, contentType) {
   const datedFileName = appendDateToFileName(fileName); // 파일명에 날짜 추가
@@ -15,11 +16,12 @@ async function uploadFileToS3(filePath, fileName, contentType) {
     Body: fileStream,
     ContentType: contentType,
   };
+
   try {
     const command = new PutObjectCommand(uploadParams);
     const response = await s3Client.send(command);
     console.log('S3에 파일이 업로드 되었습니다:', response);
-    return `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/upload/${datedFileName}`;
+    return `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/upload/${datedFileName}`;
   } catch (error) {
     console.error('S3업로드에 실패 하였습니다:', error);
     throw error;
