1aafc5f92c20e40023e904f86e120fd6bd077b4c
ChaeYi <chaeyi990731@gmail.com>
2024-03-20 12:00:17 +0900

    Fix: 오타 수정과 날짜형식 변수 설정

--- test/utils/upload/s3/appendDateToFileName.js
+++ test/utils/upload/s3/appendDateToFileName.js
@@ -1,7 +1,9 @@
-function appendDateToFileName(fileName, uploadDate = new data()) {
-  const dataPart = uploadDate.toIsSting().Split('T')[0].replace(/-/g, '');
-
-  const newFileName = `${dataPart}_${fileName}`;
+function appendDateToFileName(fileName, uploadDate = new Date()) {
+  const year = uploadDate.getFullYear();
+  const month = String(uploadDate.getMonth() + 1).padStart(2, '0');
+  const day = String(uploadDate.getDate()).padStart(2, '0');
+  const formattedDate = `${year}${month}${day}`;
+  const newFileName = `${formattedDate}_${fileName}`;
   return newFileName;
 }
 
