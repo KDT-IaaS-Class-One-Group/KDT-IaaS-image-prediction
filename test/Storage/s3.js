960696172c0bfb1a6bde3306c6d72697c4527124
Ho Nyeong <dev.honing@gmail.com>
2024-03-20 14:31:48 +0900

    Delete: 기존의 Storage 서버 로직 삭제

--- test/Storage/s3.js
+++ test/Storage/s3.js
@@ -15,34 +15,6 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION
 });
 
-// S3 버킷 리스트 조회 함수
-function listObjectsInBucket(bucketName) {
-  return new Promise((resolve, reject) => {
-    const params = {
-      Bucket: bucketName
-    };
-
-    s3.listObjects(params, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data.Contents);
-      }
-    });
-  });
-}
-
-// GET 엔드포인트 생성
-app.get('/bhn-s3', async (req, res) => {
-  try {
-    const bucketName = process.env.S3_BUCKET_NAME;
-    const objects = await listObjectsInBucket(bucketName);
-    res.json(objects);
-  } catch (error) {
-    console.error("Error listing objects in S3 bucket:", error);
-    res.status(500).json({ error: "Failed to list objects in S3 bucket" });
-  }
-});
 
 // 서버를 5555번 포트로 실행
 const PORT = 5555;
