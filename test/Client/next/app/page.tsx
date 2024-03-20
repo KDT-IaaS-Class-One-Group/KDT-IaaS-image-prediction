39b554926f3e0166c04a04e95de0f84ddbea5aaa
YoonJunHyeon <qmdldos@gmail.com>
2024-03-20 14:46:27 +0900

    test : post fetchJSON 작성

--- test/Client/next/app/page.tsx
+++ test/Client/next/app/page.tsx
@@ -3,9 +3,27 @@ import React, { useState } from "react";
 import ImageUploadForm from "@/app/component/ImageUploadForm";
 
 export default function MainPage() {
+  const handleButtonClick = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ key: "value" }),
+      });
+      // Handle the response here
+      console.log("Response : ", response);
+    } catch (error) {
+      // Handle any errors here
+      console.log("Error fetching data : ", error);
+    }
+  };
+
   return (
     <main>
       <ImageUploadForm />
+      <button onClick={handleButtonClick}>Send Fetch</button>
     </main>
   );
 }
