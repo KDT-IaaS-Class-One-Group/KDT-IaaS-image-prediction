c302d6de61b6cbc5a097593b625a7f01843109f7
Ho Nyeong <dev.honing@gmail.com>
2024-03-20 14:31:05 +0900

    Fix: 경로 주석 수정 및 업로드 컴포넌트 import

--- test/Client/next/app/next/page.tsx
+++ test/Client/next/app/next/page.tsx
@@ -1,9 +1,12 @@
-// Client/next/app/next/page.tsx
+'use client'
+// test/Client/next/app/next/page.tsx
+import ImageUploadForm from '../../components/ImageUploadForm';
 
 const NextPage = () => {
   return (
     <main>
       <h1>02 App Server(Next.js)</h1>
+      <ImageUploadForm />
     </main>
   );
 }
