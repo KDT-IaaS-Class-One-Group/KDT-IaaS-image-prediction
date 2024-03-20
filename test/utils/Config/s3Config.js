c15adc60a947c5f9891ab57b2b1bf87e19d16df5
ChaeYi <chaeyi990731@gmail.com>
2024-03-20 11:17:43 +0900

    Feat: env 경로 수정

--- test/utils/Config/s3Config.js
+++ test/utils/Config/s3Config.js
@@ -1,5 +1,5 @@
 const { S3Client } = require('@aws-sdk/client-s3');
-require('dotenv').config();
+require('dotenv').config({ path: '../../.env' });
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
