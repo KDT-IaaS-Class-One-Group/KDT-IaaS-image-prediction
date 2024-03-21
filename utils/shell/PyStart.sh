331a36931abdea011f7cbbee0f836dbc257d29ef
Ho Nyeong <dev.honing@gmail.com>
2024-03-21 16:03:29 +0900

    Feat: 가상환경이 존재하는지 탐색하는 조건문 추가

--- utils/shell/PyStart.sh
+++ utils/shell/PyStart.sh
@@ -5,3 +5,12 @@ CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # 현재 디렉토리를 콘솔에 출력
 echo "현재 디렉토리: $CURRENT_DIR"
+
+# 가상환경이 존재하는지 확인
+if [ -d "$CURRENT_DIR/.venv" ]; then
+    echo "이미 생성된 가상환경이 있습니다."
+else
+    # 가상환경이 존재하지 않는다면, 가상환경을 생성
+    python -m venv "$CURRENT_DIR/.venv"
+    echo "새로운 .venv/ 가상환경을 생성했습니다."
+fi
