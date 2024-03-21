609a7b07c137463bd4b21d79eaff2b0af1bf222b
YoonJunHyeon <qmdldos@gmail.com>
2024-03-21 17:00:58 +0900

    init : s3 관련 유틸 함수 정리

---
+++ utils/s3/uploadFileToS3.js
@@ -0,0 +1,31 @@
+// uploadFileToS3.js
+const fs = require("fs");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const s3Client = require("../../Config/s3Config");
+const appendDateToFileName = require("./appendDateToFileName");
+const bucketName = process.env.S3_BUCKET;
+
+async function uploadFileToS3(filePath, fileName, contentType) {
+  const datedFileName = appendDateToFileName(fileName); // 파일명에 날짜 추가
+  const fileStream = fs.createReadStream(filePath);
+  const uploadParams = {
+    Bucket: bucketName,
+    Key: `upload/${datedFileName}`,
+    Body: fileStream,
+    ContentType: contentType,
+  };
+
+  try {
+    const command = new PutObjectCommand(uploadParams);
+    await s3Client.send(command);
+    console.log("S3에 파일이 업로드 되었습니다:", datedFileName);
+
+    // 업로드된 파일의 경로를 반환
+    return `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/upload/${datedFileName}`;
+  } catch (error) {
+    console.error("S3업로드에 실패 하였습니다:", error);
+    throw error;
+  }
+}
+
+module.exports = { uploadFileToS3 };
