ad33ab9509b1790c7d71125651e890b3e410e455
YoonJunHyeon <qmdldos@gmail.com>
2024-03-21 17:50:43 +0900

    :fire:  Delete: utils 안쓰는 파일 삭제

--- utils/backend/queryMaker.ts
+++
@@ -1,8 +0,0 @@
-// 해당 테이블은 전체 데이터를 가져오는 쿼리를 만드는 함수입니다.
-// 예시 : SelectQueryMaker('user') => 'SELECT * FROM user'
-const SelectQueryMaker = (table) => {
-  const result = `SELECT * FROM ${table}`;
-  return result;
-};
-
-export default SelectQueryMaker;
