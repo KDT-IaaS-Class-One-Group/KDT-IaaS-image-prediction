48c04e15e7b56fb40e1ab5f53feee5b7a0b51a8a
Ho Nyeong <dev.honing@gmail.com>
2024-03-22 15:09:43 +0900

    Feat: /upload 라우트 추가

--- test/Storage/s3.py
+++ test/Storage/s3.py
@@ -4,9 +4,15 @@ from fastapi import FastAPI
 # FastAPI 인스턴스 생성
 app = FastAPI()
 
+# 루트 경로에 대한 라우트
 @app.get("/")
 def read_root():
     return {"Greet": "Hello, FastAPI World!"}
+
+# 파일 업로드를 위한 라우트
+@app.get("/upload")
+def upload_file():
+    return {"Upload": "파일 업로드"}
 
 # 서버를 5555번 포트로 실행
 if __name__ == "__main__":
