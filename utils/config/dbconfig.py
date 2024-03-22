68cf9794d7c631d472241083eec564cb75a0a0d6
YoonJunHyeon <qmdldos@gmail.com>
2024-03-22 14:11:58 +0900

    :sparkles: Feat: .env 생성 및 설정

--- utils/config/dbconfig.py
+++ utils/config/dbconfig.py
@@ -1,29 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# 선택1
-# dbConfig = {
-#   "host": os.environ.get("DB_HOST", ""),
-#   "user": os.environ.get("DB_USER", ""),
-#   "password": os.environ.get("DB_PASSWORD", ""),
-#   "database": os.environ.get("DB_NAME", ""),
-# }
+load_dotenv("../../.env")
 
-
-# 선택2 .env
-# .env 파일 로드
-load_dotenv()
-
-# 환경 변수 가져오기
-db_host = os.getenv("DB_HOST")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_name = os.getenv("DB_NAME")
-
-# dbConfig 딕셔너리 생성
-dbConfig = {
-  "host": db_host,
-  "user": db_user,
-  "password": db_password,
-  "database": db_name
+db_config = {
+    "host": os.getenv("db_config.host"),
+    "port": os.getenv("db_config.port"),
+    "user": os.getenv("db_config.user"),
+    "password": os.getenv("db_config.password"),
+    "database": os.getenv("db_config.database")
 }
