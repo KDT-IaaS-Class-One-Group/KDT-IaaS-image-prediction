2f41c1b99906410e59288e2f30170b8a749f2956
Ho Nyeong <dev.honing@gmail.com>
2024-03-22 12:10:43 +0900

    Feat: 사용자가 source 명령을 실행하도록 가이드

--- utils/shell/PyStart.sh
+++ utils/shell/PyStart.sh
@@ -9,6 +9,8 @@ echo "현재 디렉토리: $CURRENT_DIR"
 # 가상환경이 존재하는지 확인
 if [ -d "$CURRENT_DIR/.venv" ]; then
     echo "이미 생성된 가상환경이 있습니다."
+    # 가상환경 실행 안내
+    echo "source .venv/Scripts/activate를 입력해 가상환경을 실행하세요."
 else
     # 가상환경이 존재하지 않는다면, 가상환경을 생성
     python -m venv "$CURRENT_DIR/.venv"
