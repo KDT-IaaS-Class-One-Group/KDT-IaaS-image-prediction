c46a357b693bf1d4bdacfb613c085f2b1f1d2362
Ho Nyeong <dev.honing@gmail.com>
2024-03-22 14:05:05 +0900

    Feat: FastAPI 서버 제작

--- test/Storage/s3.py
+++ test/Storage/s3.py
@@ -1,2 +1,14 @@
 # test/Storage/s3.py
+from fastapi import FastAPI
 
+# FastAPI 인스턴스 생성
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Greet": "Hello, FastAPI World!"}
+
+# 서버를 5555번 포트로 실행
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=5555)
