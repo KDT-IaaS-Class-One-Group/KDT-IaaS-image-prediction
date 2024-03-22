36268fea6989edeb76c3f6129ab747d22db1070a
Ho Nyeong <dev.honing@gmail.com>
2024-03-22 15:20:04 +0900

    Feat: S3 연결 설정

--- test/Storage/s3.py
+++ test/Storage/s3.py
@@ -1,5 +1,18 @@
 # test/Storage/s3.py
 from fastapi import FastAPI
+import boto3
+from dotenv import load_dotenv
+
+# 환경변수 로드
+load_dotenv()
+
+# S3 연결 설정
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION_NAME"),
+)
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
