7ab79cb195945990a4c668c9b471c68b2445518b
kongukjae <kongukjae@gmail.com>
2024-03-25 15:25:34 +0900

    Layout.tsx에 존재하는 리터럴 데이터들을 모아놓은 파일 예제

---
+++ example/Client/next/src/app/layout-metadata.ts
@@ -0,0 +1,18 @@
+const layoutMetadata = {
+  default : {
+    title : "Project B 인터랙션 점검",
+    description : "Next.js로 확인",
+    language : "ko",
+  },
+  "style" : {
+    padding: "1rem",
+    header : {
+      backgroundColor : "lightblue",
+    },
+    footer : {
+      backgroundColor : "ghostwhite",
+    },
+  },
+}
+
+export default layoutMetadata;
