203821d9eb4f3a093fea50fbfc1ffa8855ce2ded
YoonJunHyeon <qmdldos@gmail.com>
2024-03-25 12:46:11 +0900

    Bug : 여전히 작동되지 않는 pydanic

--- test/DB/db.py
+++ test/DB/db.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import mysql.connector
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,30 +20,43 @@ app.add_middleware(
 
 mydb = mysql.connector.connect(**db_config)
 
+#이미지 메타데이터를 저장하기 위한 요청 바디 모델
+class ImageMetadata(BaseModel):
+    """
+    Represents the metadata of an image.
+    
+    Attributes:
+        filename (str): The name of the image file.
+        filepath (str): The path of the image file.
+        filesize (int): The size of the image file in bytes.
+    """
+    filename: str
+    filepath: str
+    filesize: int
+    
+
 @app.get('/')
 async def read_root():
     return {"Hello": "im 8001"}
 
-# 이미지 메타데이터를 저장하기 위한 요청 바디 모델
-# class ImageMetadata(BaseModel):
-#     filename: str
-#     filepath: str
-#     filesize: int
-
 # 이미지 메타데이터를 저장하는 라우트 함수
 @app.post("/save-image-metadata")
-async def save_image_metadata(image_metadata: ImageMetadata):
+async def save_image_metadata(image_metadata:ImageMetadata):
     try:
-        # 받은 이미지 메타데이터를 데이터베이스에 저장
-        # 이 부분은 실제 데이터베이스 연결 및 저장 로직으로 대체되어야 함
         # 여기서는 받은 데이터를 그대로 출력하는 예시 코드를 작성하였음
         print("Received image metadata:", image_metadata.dict())
-
+        
+        # 받은 이미지 메타데이터를 데이터베이스에 저장
+        cursor = mydb.cursor()
+        query = "INSERT INTO image_metadata (filename, filepath, filesize) VALUES (%s, %s, %s)"
+        values = (image_metadata.filename, image_metadata.filepath, image_metadata.filesize)
+        cursor.execute(query, values)
+        mydb.commit()
+        
         return {"message": "Image metadata saved successfully"}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to save image metadata: {str(e)}")
-
 
 # 테이블 이름 가져오기
 @app.get("/table")
