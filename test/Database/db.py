bc48d96ad22c9cf1d6a16a708c0d119dc54b778e
Ho Nyeong <dev.honing@gmail.com>
2024-03-25 11:14:21 +0900

    Setup: 로컬 MariaDB 연결 설정

--- test/Database/db.py
+++ test/Database/db.py
@@ -1,9 +1,21 @@
 # Database/db.py
 
 from fastapi import FastAPI
+import pymysql
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
+
+# MariaDB 연결 설정
+db = pymysql.connect(
+  host='localhost',
+  user='root',
+  password='1234',
+  db='image',
+)
+
+# 커서 생성
+cursor = db.cursor(pymysql.cursors.DictCursor)
 
 @app.get("/")
 def read_root():
