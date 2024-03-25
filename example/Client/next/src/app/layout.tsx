c88a98865e7fa2f7f8e902820a67d0e27b04c920
kongukjae <kongukjae@gmail.com>
2024-03-25 16:26:16 +0900

    explain : 해당 커밋까지 동료들과 협의가 완료되었다면 수정작업은 모두 layoutMetadata라는 값에서 조정가능 마치 변수처럼

--- example/Client/next/src/app/layout.tsx
+++ example/Client/next/src/app/layout.tsx
@@ -3,23 +3,30 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 // custom import
+// notice : layoutMetadata
+// info: title, description, language, style(padding, header, footer) 등의 값이 객체로 작성되어 있음
 import layoutMetadata from "./layout-metadata";
+// compoenent import
+import Header from "../../components/atoms/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: layoutMetadata.default.title,
-  description: layoutMetadata.default.description,
+  description: layoutMetadata.default.description
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header style={{ ...layoutMetadata.style.header, padding: layoutMetadata.style.padding }}>
+        </Header>
+      </body>
     </html>
   );
 }
