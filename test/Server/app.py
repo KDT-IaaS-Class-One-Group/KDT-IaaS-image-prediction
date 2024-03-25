4183fe0871f31e94f45e6f5665debce57c45ba00
YoonJunHyeon <qmdldos@gmail.com>
2024-03-25 13:12:30 +0900

    :alembic: Test: response를 제공 그러나 실패
    현상 : 계속된 오류 상태 500 반환
    시도 : db저장 로직을 빼고, 기존 라우터와 동일하게 작성되고, 메타데이터 추출만 해보려고 시도했으나 계속된 오류 메세지를 추출한다.

--- test/Server/app.py
+++ test/Server/app.py
@@ -34,28 +34,33 @@ async def upload_image_and_save_to_db(image: UploadFile = File(...)):
         # 이미지를 받아옴
         image_content = await image.read()
 
-        # storage 서버로 전송
+        # * storage 서버로 전송
         storage_url = "http://localhost:8002/check"  # storage 서버의 엔드포인트 URL
         files = {"image": (image.filename, image_content)}  # 이미지 파일을 담은 딕셔너리
-        response = await httpx.post(storage_url, files=files)  # storage 서버로 POST 요청 보냄
+        response = httpx.post(storage_url, files=files)  # storage 서버로 POST 요청 보냄
+        print("storage : " + response.json())
         response.raise_for_status()  # 오류 발생 시 예외를 일으킴
 
-        # 이미지 파일의 메타데이터 추출
-        image_metadata = {
-            "filename": image.filename,
-            "filepath": response.json().get("filepath"),  # storage 서버에서 받은 파일 경로
-            "filesize": image.filesize,
-        }
-        print(image_metadata)
-        # DB에 이미지 메타데이터 저장
-        db_url = "http://localhost:8001/save-image-metadata"  # DB 서버의 엔드포인트 URL
-        db_response = await httpx.post(db_url, json=image_metadata)  # DB 서버로 POST 요청 보냄
-        db_response.raise_for_status()  # 오류 발생 시 예외를 일으킴
+        # * 이미지 파일의 메타데이터 추출
+        print(image)
+        
+        # image_metadata = {
+        #     "filename": image.filename,
+        #     # "filepath": response.json().get("filepath"),  # storage 서버에서 받은 파일 경로
+        #     "filepath": image.filename,  # storage 서버에서 받은 파일 경로
+        #     "filesize": image.filesize,
+        # }
+        # print(image_metadata)
+        
+        # * DB에 이미지 메타데이터 저장
+        # db_url = "http://localhost:8001/save-image-metadata"  # DB 서버의 엔드포인트 URL
+        # db_response = httpx.post(db_url, json=image_metadata)  # DB 서버로 POST 요청 보냄
+        # db_response.raise_for_status("db error")  # 오류 발생 시 예외를 일으킴
 
         return {"message": "이미지가 성공적으로 업로드되고 메타데이터가 DB에 저장되었습니다."}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"이미지 업로드 및 메타데이터 저장에 실패했습니다: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"이미지 업로드 및 메타데이터 저장에 실패했습니다: {e}")
 
 # 클라이언트로부터 이미지를 받아서 storage 서버로 전달하는 엔드포인트
 @app.post("/check")
